ab86a0f86cc46f06fd9aa9c78609a601d2028f8a
shobhit-jain <shobhitjain.work@gmail.com>
2023-07-22 21:50:26 +0530

    initial-commit

---
+++ server.ts
@@ -0,0 +1,162 @@
+// // Virtual entry point for the app
+// import * as remixBuild from '@remix-run/dev/server-build';
+// import {
+//   createRequestHandler,
+//   getStorefrontHeaders,
+// } from '@shopify/remix-oxygen';
+// import {createStorefrontClient, storefrontRedirect} from '@shopify/hydrogen';
+
+// import {HydrogenSession} from '~/lib/session.server';
+// import {getLocaleFromRequest} from '~/lib/utils';
+
+// /**
+//  * Export a fetch handler in module format.
+//  */
+// export default {
+//   async fetch(
+//     request: Request,
+//     env: Env,
+//     executionContext: ExecutionContext,
+//   ): Promise<Response> {
+//     try {
+//       /**
+//        * Open a cache instance in the worker and a custom session instance.
+//        */
+//       if (!env?.SESSION_SECRET) {
+//         throw new Error('SESSION_SECRET environment variable is not set');
+//       }
+
+//       const waitUntil = (p: Promise<any>) => executionContext.waitUntil(p);
+//       const [cache, session] = await Promise.all([
+//         caches.open('hydrogen'),
+//         HydrogenSession.init(request, [env.SESSION_SECRET]),
+//       ]);
+
+//       /**
+//        * Create Hydrogen's Storefront client.
+//        */
+//       const {storefront} = createStorefrontClient({
+//         cache,
+//         waitUntil,
+//         i18n: getLocaleFromRequest(request),
+//         publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+//         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+//         storeDomain: env.PUBLIC_STORE_DOMAIN,
+//         storefrontId: env.PUBLIC_STOREFRONT_ID,
+//         storefrontHeaders: getStorefrontHeaders(request),
+//       });
+
+//       /**
+//        * Create a Remix request handler and pass
+//        * Hydrogen's Storefront client to the loader context.
+//        */
+//       const handleRequest = createRequestHandler({
+//         build: remixBuild,
+//         mode: process.env.NODE_ENV,
+//         getLoadContext: () => ({
+//           session,
+//           waitUntil,
+//           storefront,
+//           env,
+//         }),
+//       });
+
+//       const response = await handleRequest(request);
+
+//       if (response.status === 404) {
+//         /**
+//          * Check for redirects only when there's a 404 from the app.
+//          * If the redirect doesn't exist, then `storefrontRedirect`
+//          * will pass through the 404 response.
+//          */
+//         return storefrontRedirect({request, response, storefront});
+//       }
+
+//       return response;
+//     } catch (error) {
+//       // eslint-disable-next-line no-console
+//       console.error(error);
+//       return new Response('An unexpected error occurred', {status: 500});
+//     }
+//   },
+// };
+
+// Virtual entry point for the app
+import * as remixBuild from '@remix-run/dev/server-build';
+import {createRequestHandler} from '@remix-run/server-runtime';
+import {createStorefrontClient} from '@shopify/hydrogen';
+
+import {HydrogenSession} from '~/lib/session.server';
+import {getLocaleFromRequest} from '~/lib/utils';
+
+/**
+ * Export a fetch handler in module format.
+ */
+export default async function (request: Request) {
+  try {
+    /**
+     * This has to be done so messy because process.env can't be destructured
+     * and only variables explicitly named are present inside a Vercel Edge Function.
+     * See https://github.com/vercel/next.js/pull/31237/files
+     */
+    const env: Env = {
+      SESSION_SECRET: '',
+      PUBLIC_STOREFRONT_API_TOKEN: '',
+      PRIVATE_STOREFRONT_API_TOKEN: '',
+      PUBLIC_STORE_DOMAIN: '',
+      PUBLIC_STOREFRONT_ID: '',
+    };
+    env.SESSION_SECRET = process.env.SESSION_SECRET;
+    env.PUBLIC_STOREFRONT_API_TOKEN = process.env.PUBLIC_STOREFRONT_API_TOKEN;
+    env.PRIVATE_STOREFRONT_API_TOKEN = process.env.PRIVATE_STOREFRONT_API_TOKEN;
+    env.PUBLIC_STORE_DOMAIN = process.env.PUBLIC_STORE_DOMAIN;
+    /**
+     * Open a cache instance in the worker and a custom session instance.
+     */
+    if (!env?.SESSION_SECRET) {
+      throw new Error('SESSION_SECRET process.environment variable is not set');
+    }
+
+    const [session] = await Promise.all([
+      HydrogenSession.init(request, [process.env.SESSION_SECRET]),
+    ]);
+
+    /**
+     * Create Hydrogen's Storefront client.
+     */
+    const {storefront} = createStorefrontClient({
+      buyerIp: request.headers.get('x-forwarded-for') ?? undefined,
+      i18n: getLocaleFromRequest(request),
+      publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+      privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+      storeDomain: env.PUBLIC_STORE_DOMAIN,
+      storefrontId: env.PUBLIC_STOREFRONT_ID,
+      // storefrontId: process.env.PUBLIC_STOREFRONT_ID,
+      // requestGroupId: request.headers.get('request-id'),
+    });
+
+    const handleRequest = createRequestHandler(remixBuild as any, 'production');
+
+    const response = await handleRequest(request, {
+      session,
+      storefront,
+      env,
+      waitUntil: () => Promise.resolve(),
+    });
+
+    if (response.status === 404) {
+      /**
+       * Check for redirects only when there's a 404 from the app.
+       * If the redirect doesn't exist, then `storefrontRedirect`
+       * will pass through the 404 response.
+       */
+      // return storefrontRedirect({request, response, storefront});
+    }
+
+    return response;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return new Response('An unexpected error occurred', {status: 500});
+  }
+}
